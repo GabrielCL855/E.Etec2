b037542f2edb521e89cc53c26ce7a6ea113a04be
Gabriel Cabral <G.Cabral>
2020-09-07 21:21:39 -0300

    Adiciona a resposta do ecercício

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E.Etec2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá usuário, eu me chamo Gabriel Cabral!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,4 +5,11 @@ C# O Iniciou:
 
 Faça um programa que exiba `Olá usuário, eu me chamo Ermogenes!` (utilize o seu nome).
 
+**Como execultar o  programa**
+
+Acesse a pasta do projeto:
+
+``` 
+dotnet run
+```
 
